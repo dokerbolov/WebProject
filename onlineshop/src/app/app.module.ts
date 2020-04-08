4ed1796886822c726dc4b11738691e045430a9ca
yesworld000 <damir.bulegenov.00.kz@gmail.com>
2020-04-09 00:13:33 +0600

    Damir

--- onlineshop/src/app/app.module.ts
+++ onlineshop/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './Products/Products.component';
 import { ProductsDetailComponent } from './ProductsDetail/ProductsDetail.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { MenProductsComponent } from './men-products/men-products.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
@@ -15,11 +18,24 @@ import { MenProductsComponent } from './men-products/men-products.component';
       ProductsComponent,
       ProductsDetailComponent,
       CategoriesComponent,
-      MenProductsComponent
+      MenProductsComponent,
+      LoginComponent,
+      RegistrationComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule.forRoot([
+         {
+            path: 'login',
+            component: LoginComponent
+         },
+         {
+            path: 'registration',
+            component: RegistrationComponent
+         }
+      ])
    ],
    providers: [],
    bootstrap: [
